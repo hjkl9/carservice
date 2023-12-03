9d7c5ee56829226d950d50ae2b40a8f52d8135c8
Illusion <1161524405@qq.com>
2023-12-03 17:47:25 +0800

    first commit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module carservice
+
+go 1.21.4
---
+++ internal/logic/phonenumberloginlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"carservice/internal/svc"
+	"carservice/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PhoneNumberLoginLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewPhoneNumberLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PhoneNumberLoginLogic {
+	return &PhoneNumberLoginLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *PhoneNumberLoginLogic) PhoneNumberLogin(req *types.PhoneNumberLoginReq) (resp *types.PhoneNumberLoginRep, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}

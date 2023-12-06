2fb081269fa10b3890d30234523506da8615581b
Illusion <1161524405@qq.com>
2023-12-06 20:54:11 +0800

    新增手机号段

--- internal/pkg/sms/validate.go
+++ internal/pkg/sms/validate.go
@@ -6,6 +6,6 @@ import (
 
 func CheckPhoneNumber(phoneNumber string) bool {
 	// Check prefix of phone number.
-	matched, _ := regexp.MatchString("0?(13|14|15|18)[0-9]{9}", phoneNumber)
+	matched, _ := regexp.MatchString("0?(13|14|15|17|18)[0-9]{9}", phoneNumber)
 	return matched
 }

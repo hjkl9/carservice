aa6c01c20015703e203f530b5a474bbefc91dfd1
Illusion <1161524405@qq.com>
2023-12-08 19:12:30 +0800

    user table ddl

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,2 +1,15 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255),
+  `avatar_url` VARCHAR(255),
+  `password` VARCHAR(255),
+  `phone_number` VARCHAR(255),
+  `locked` tinyint(1) DEFAULT '0',
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_users_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 -- todo: write ddl.

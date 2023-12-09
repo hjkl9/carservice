acebfab9efa9dfa536e6ee8f65a1a751ceeec0e9
Illusion <1161524405@qq.com>
2023-12-09 09:41:59 +0800

    read config from file.

--- main.rs
+++ main.rs
@@ -1,4 +1,12 @@
+use std::io;
+use std::File;
 
 fn main() {
     // initialization
+}
+
+fn read_config_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("./etc/carservice.yaml")?.read_to_string(&mut s)?;
+    Ok(s)
 }

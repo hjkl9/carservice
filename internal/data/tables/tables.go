d964218ad430544daf5a9d626b73b5e416fa4cde
Illusion <1161524405@qq.com>
2023-12-20 19:32:48 +0800

    创建数据表枚举文件

---
+++ internal/data/tables/tables.go
@@ -0,0 +1,12 @@
+package tables
+
+// 数据表枚举
+const (
+	User           = "members"
+	UserBind       = "member_binds"
+	CarBrand       = "car_brands"
+	CarBrandSeries = "car_brand_series"
+	CarOwnerInfo   = "car_owner_infos"
+	PartnerStore   = "partner_stores"
+	UserOrder      = "user_orders"
+)

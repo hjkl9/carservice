b89c403d6af52a6e26ee22d89dc03effad0d9ae5
Lucas chen <lucas.chen@gbo-asia.com>
2023-12-28 10:38:43 +0800

    预处理查询语句

--- internal/logic/carbrand/optionlistlogic.go
+++ internal/logic/carbrand/optionlistlogic.go
@@ -27,7 +27,10 @@ func NewOptionListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Option
 func (l *OptionListLogic) OptionList() (resp []types.CarBrandOptionListItem, err error) {
 	sql := "SELECT `brand_id` AS `id`, `brand_name` AS `label`, `pinyin` AS `pinyin` FROM `car_brands` WHERE `business_status` = ? ORDER BY `pinyin` ASC"
 	var list []types.CarBrandOptionListItem
-	err = l.svcCtx.DBC.Select(&list, sql, "1")
+	// 预处理查询语句
+	stmt, err := l.svcCtx.DBC.PreparexContext(l.ctx, sql)
+	stmt.SelectContext(l.ctx, &list, "1")
+	// err = l.svcCtx.DBC.Select(&list, sql, "1")
 	if err != nil {
 		return nil, errcode.InternalServerError.SetMsg("查询数据时发生错误").SetDetails(err.Error())
 	}

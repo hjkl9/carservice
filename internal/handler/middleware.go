0115dcb9f6f100f2b47e78bfa5c47b0d76ca2673
illusion <1161524405@qq.com>
2023-12-31 01:19:21 +0800

    不检查数据库连接状态

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,25 +1,25 @@
 package handler
 
 import (
-	"carservice/internal/pkg/common/errcode"
-	stdresponse "carservice/internal/pkg/httper/response"
+	// "carservice/internal/pkg/common/errcode"
+	// stdresponse "carservice/internal/pkg/httper/response"
 	"carservice/internal/svc"
-	"net/http"
+	// "net/http"
 
-	"github.com/zeromicro/go-zero/core/logc"
+	// "github.com/zeromicro/go-zero/core/logc"
 	"github.com/zeromicro/go-zero/rest"
 )
 
 func RegisterGlobalMiddleware(server *rest.Server, serverCtx *svc.ServiceContext) {
-	server.Use(func(next http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			err := serverCtx.DBC.Ping()
-			if err != nil {
-				logc.Errorf(r.Context(), "MySQL 连接发生错误或配置不正确, err: %#v\n", err)
-				stdresponse.ResponseWithCtx(r.Context(), w, errcode.New(http.StatusInternalServerError, "-", "MySQL 连接发生错误或配置不正确"))
-				return
-			}
-			next(w, r)
-		}
-	})
+	// server.Use(func(next http.HandlerFunc) http.HandlerFunc {
+	// 	return func(w http.ResponseWriter, r *http.Request) {
+	// 		err := serverCtx.DBC.Ping()
+	// 		if err != nil {
+	// 			logc.Errorf(r.Context(), "MySQL 连接发生错误或配置不正确, err: %#v\n", err)
+	// 			stdresponse.ResponseWithCtx(r.Context(), w, errcode.New(http.StatusInternalServerError, "-", "MySQL 连接发生错误或配置不正确"))
+	// 			return
+	// 		}
+	// 		next(w, r)
+	// 	}
+	// })
 }

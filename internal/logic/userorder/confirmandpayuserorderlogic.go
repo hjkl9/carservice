f6a95ac31dc5de7988024c2a6cae5e6767bacf95
Lucas chen <lucas.chen@gbo-asia.com>
2024-01-11 10:35:57 +0800

    add log pinters.

--- internal/logic/userorder/confirmandpayuserorderlogic.go
+++ internal/logic/userorder/confirmandpayuserorderlogic.go
@@ -46,34 +46,30 @@ func (l *ConfirmAndPayUserOrderLogic) ConfirmAndPayUserOrder(req *types.ConfirmA
 		return errcode.OrderNotFoundErr
 	}
 
-	// 获取当前订单状态
-	var orderStatus uint8
-	query := "SELECT `order_status` `orderStatus` FROM `user_orders` WHERE `id` = ? LIMIT 1"
-	stmt, err := l.svcCtx.DBC.PreparexContext(l.ctx, query)
-	if err != nil {
-		return errcode.DatabasePrepareErr
-	}
-	if err = stmt.GetContext(l.ctx, &orderStatus, req.Id); err != nil {
-		return errcode.DatabaseGetErr
-	}
-
-	// 是否符合更改状态
-	if orderStatus != userorder.Pending {
-		return errcode.ConfirmedOrderErr.SetMessage("不符合确认和支付用户订单的条件")
-	}
-
 	var order struct {
 		OrderNumber string  `db:"orderNumber"`
 		Amount      float64 `db:"amount"`
+		OrderStatus uint8   `db:"orderStatus"`
 		OpenId      string  `db:"openid"` // todo: relation query
 	}
-	query = "SELECT `user_orders`.`order_number` `orderNumber`, `user_orders`.`act_amount` `amount`, `user_binds`.`open_id` `openid` FROM `user_orders` JOIN `user_binds`.`user_id` = `user_orders`.`member_id` WHERE `user_orders`.`id` = ? LIMIT 1"
-	stmt, err = l.svcCtx.DBC.PreparexContext(l.ctx, query)
+	query := "SELECT `user_orders`.`order_number` `orderNumber`, `user_orders`.`act_amount` `amount`, `user_binds`.`open_id` `openid` FROM `user_orders` JOIN `user_binds`.`user_id` = `user_orders`.`member_id` WHERE `user_orders`.`id` = ? LIMIT 1"
+	stmt, err := l.svcCtx.DBC.PreparexContext(l.ctx, query)
 	if err != nil {
+		logc.Errorf(l.ctx, "预处理查询订单时发生错误, err: %s\n", err.Error())
 		return errcode.DatabasePrepareErr.WithDetails(err.Error())
 	}
 	if err = stmt.GetContext(l.ctx, &order, req.Id); err != nil {
+		logc.Errorf(l.ctx, "查询订单时发生错误, err: %s\n", err.Error())
 		return errcode.DatabaseGetErr.WithDetails(err.Error())
+	}
+
+	// 是否符合更改状态
+	if order.OrderStatus != userorder.Pending && order.Amount == 0 {
+		return errcode.ConfirmedOrderErr.SetMessage("不符合确认和支付用户订单的条件")
+	}
+
+	pcfg := payment.PaymentConfig{
+		// todo: waiting the mch config to test the payment service.
 	}
 
 	// 开始支付
@@ -85,7 +81,7 @@ func (l *ConfirmAndPayUserOrderLogic) ConfirmAndPayUserOrder(req *types.ConfirmA
 		Amount:      int64(order.Amount),
 		OpenId:      order.OpenId,
 	}
-	if err = payment.JsApiOrder(payment.PaymentConfig{ /* TODO */ }, payload); err != nil {
+	if err = payment.JsApiOrder(pcfg, payload); err != nil {
 		return errcode.OrderConfirmAndPayErr.WithDetails(err.Error())
 	}
 

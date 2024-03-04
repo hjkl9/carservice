eff5c2eb6f25c8cfa5e0cfc604403c6ae06a5391
Illusion <1161524405@qq.com>
2024-03-04 21:14:45 +0800

    修复返回错误的 error 实体

--- internal/handler/user/wechatauthorizationhandler.go
+++ internal/handler/user/wechatauthorizationhandler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"carservice/internal/logic/user"
+	"carservice/internal/pkg/common/errcode"
 	"carservice/internal/pkg/httper/api"
 	"carservice/internal/svc"
 	"carservice/internal/types"
@@ -15,7 +16,7 @@ func WechatAuthorizationHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.WechatAuthorizationReq
 		if err := httpx.Parse(r, &req); err != nil {
-			api.ResponseWithCtx(r.Context(), w, nil, err)
+			api.ResponseWithCtx(r.Context(), w, nil, errcode.InvalidParametersErr)
 			// stdresponse.ResponseWithCtx(r.Context(), w, nil, errcode.New(http.StatusBadRequest, "feature.", err.Error()))
 			return
 		}
